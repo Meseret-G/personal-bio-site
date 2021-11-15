d82aa2eb7e443eb6ea9eca3f20bbecd2b0bf9237
Messi_G <negash4@gmail.com>
2021-11-14 23:51:29 -0600

    firebase authentication completed

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,18 @@
-// index for router
+import React from 'react';
+import { Routes, Switch } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const index = ({ user }) => (
+  <div>
+    <Switch>
+      <Routes user={user} />
+    </Switch>
+  </div>
+);
+
+index.propTypes = {
+  user: PropTypes.shape(PropTypes.obj),
+};
+index.defaultProps = { user: null };
+
+export default index;
