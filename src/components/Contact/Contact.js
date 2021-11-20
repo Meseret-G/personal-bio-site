bdc2be880d5ef13f25249ad90f0db303993b93ee
Messi_G <negash4@gmail.com>
2021-11-20 12:06:47 -0600

    tech page

--- src/components/Contact/Contact.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './contact.scss';
-
-export default function Contact() {
-  return <div className="contact">Contact Me</div>;
-}
--- src/components/Technologies/Technologies.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './technologies.scss';
-
-export default function Technologies() {
-  return <div className="tech">Technologies</div>;
-}
