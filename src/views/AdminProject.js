b9537df8012871827736add0e7b3e290bc585b3e
Messi_G <negash4@gmail.com>
2021-11-23 00:55:13 -0600

    some styling

--- src/views/AdminProject.js
+++ src/views/AdminProject.js
@@ -1,45 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useParams, useHistory } from 'react-router-dom';
-import { Button } from 'reactstrap';
 import { getSingleProject } from '../Helpers/ProjectData';
 import AdminProjectForm from '../components/AdminComponents/AdminProjectForm';
 import { signOutUser } from '../api/auth';
 
-export default function AdminEditView({ user }) {
-  const [editProject, setEditProject] = useState({});
-  const { firebaseKey } = useParams();
+export default function AdminProject({ user }) {
+  const [editItem, setEditItem] = useState({});
+  const { key } = useParams();
   const history = useHistory();
 
   useEffect(() => {
     let isMounted = true;
     if (isMounted) {
-      getSingleProject(firebaseKey).then(setEditProject);
+      getSingleProject(key).then(setEditItem);
     }
     return () => {
       isMounted = false;
     };
   }, []);
-
   return (
     <div>
-      <AdminProjectForm project={editProject} user={user} />
-      <Button
+      <AdminProjectForm obj={editItem} user={user} />
+      <button
         type="button"
         onClick={() => {
           signOutUser().then(() => {
             history.push('/');
           });
         }}
-      />
-      SignOut
+      >
+        Sign Out
+      </button>
     </div>
   );
 }
-
-AdminEditView.propTypes = {
+AdminProject.propTypes = {
   user: PropTypes.shape(PropTypes.obj),
 };
-AdminEditView.defaultProps = {
+AdminProject.defaultProps = {
   user: null,
 };
