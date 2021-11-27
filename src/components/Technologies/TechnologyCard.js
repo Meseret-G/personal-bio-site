42e48de262c29815da787dc1f96c8946318deabf
Messi_G <negash4@gmail.com>
2021-11-26 19:34:07 -0600

    more styling for on tech page

--- src/components/Technologies/TechnologyCard.js
+++ src/components/Technologies/TechnologyCard.js
@@ -3,16 +3,14 @@ import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-  },
+  // root: {
+  //   maxWidth: 345,
+  // },
   media: {
     height: 140,
   },
@@ -22,28 +20,22 @@ export default function Technologies({ tech }) {
   const classes = useStyles();
 
   return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image={tech.imageUrl}
-          title="logo"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {tech.name}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {tech.description}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
+    <div className="techdiv">
+      <Card className="techcard">
+        <CardActionArea className="cardarea">
+          <CardMedia
+            className={classes.media}
+            image={tech.imageUrl}
+            title="logo"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {tech.name}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </div>
   );
 }
 Technologies.propTypes = {
